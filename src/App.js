35b49f21c4bf7d17aca688ce67620ca72fd827f0
PraiseKemi <160168260+PraiseKemi@users.noreply.github.com>
2024-11-06 15:59:00 +0100

    created and styled the hero section of the homepage using CSS and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import heroimage from "./home-hero-section.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
+      <header className="AppHeader">
+          <div className="row g-0">
+            <div className="heroText col-md-6">
+              <h1>IGNITING THE YOUNG FOR <span className="highlight">CHRIST</span></h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                <br />
+                eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </p>
+              <button className="communityButton">Join Community</button>
+              <button className="learnButton">Learn more</button>
+            </div>
+            <div className="heroImage col-md-6">
+              <img src={heroimage} alt="girls praying" className="img-fluid float-end" />
+            </div>
+          </div>
       </header>
     </div>
   );
